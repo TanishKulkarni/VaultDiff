1d8c4258fd00bf21b9ff19cafcfdb336994834f6
Tanish Kulkarni <learntanish123@gmail.com>
2026-01-17 22:04:50 +0530

    Prepare backend for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
 // Multer setup (in-memory storage)
